3f79e3873333d24822262fde9a7503f7d0c49b72
Kevin <vnctkevin@gmail.com>
2023-04-28 22:28:28 +0700

    add cart functionality

---
+++ assets/js/cart.js
@@ -0,0 +1,22 @@
+//fetch from https://final-project-api-lugr.onrender.com/order everytime the page is loaded
+//loop through the output in the html id order-cards
+//implements a cancel button as well
+
+const orderCards = document.getElementById('order-cards');
+
+fetch('https://final-project-api-lugr.onrender.com/order')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(order => {
+            orderCards.innerHTML += `
+            <div class="card-body">
+                <h5 class="card-title text-dark">${order.tipe_paket}</h5>
+                <p class="card-text">${order.nama_pasangan_pengantin}</p>
+                <a href="#" class="btn btn-danger">Cancel</a>
+            </div>
+            </div>
+            `
+        });
+    }
+    )
+    .catch(error => console.error('Error:', error));   
